d3daed0a04a183cded3c7186ab2515d4409ef2a2
Saba-Developer <sabamadina87@gmail.com>
2025-04-25 12:03:27 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+let first_num=parseFloat(prompt("Enter 1st number = "));
+let sec_num=parseFloat(prompt("Enter Second number = "));
+console.log(`The Sum of ${first_num} and ${sec_num} = ${first_num + sec_num}`);
+
